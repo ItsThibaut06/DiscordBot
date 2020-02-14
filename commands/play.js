0c6ce73ab4366c684e5648325b0f2c6a04efd91e
ItsThibaut06 <thibaut.prive123@gmail.com>
2020-02-14 21:26:10 +0100

    init

--- commands/play.js
+++ commands/play.js
@@ -1,20 +1,32 @@
-const discord = require('discord.js');
+const ytdl = require("ytdl.core");
 
 module.exports.run = (bot, message, args) => {
-    
- // PlayInfo embed
- var PlayInfo = new discord.RichEmbed()
+   
+   if(!message.member.voiceChannel) return message.channel.send("Je zit niet in een spraakkanaal!");
 
- .setColor('#ff0000')
- .setDescription("Muziekbot nog in de maak!")
- .setTimestamp()
- .setFooter("Tryhard Bot")
- .setTimestamp();
+   if(message.guild.me.voiceChannel) return message.channel.send("Join mijn spraakkanaal!");
 
-return message.channel.send(PlayInfo);
+   if(!args[0]) return message.channel.send("Geef een URL link op!");
+
+   var validate = await ytdl.validateURL(args[0]);
+
+   if(!validate) return message.channel.send("Geef een juiste URL op!");
+
+   var info = await ytdl.getInfo(args[0]);
+
+   var options = { seek: 0, volume: 1};
+
+   var voiceConnection = message.member.voiceChannel.join()
+   .than(voiceChannel => {
+       var stream = ytdl(args[0], { filter: "audioonly"});
+       var streamDispatch = voiceChannel.playStream(stream, options);
+   })
+   .catch(console.error);
+
+   message.channel.send(`Nu aan het speler ${info.title}`)
 
 }
 
 module.exports.help = {
     name: 'play'
-}
+};
